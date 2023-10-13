b0d426c3abe97eb2bc1ff3213dbea089e62d8c23
ALESSANDRO CAPILUPPI <115798135+Yatorimi@users.noreply.github.com>
2023-10-13 16:58:41 +0200

    ajout export et gomme

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const table = document.getElementById('table');
 const resetButton = document.getElementById('resetButton');
+const eraseButton = document.getElementById('eraseButton');
+const saveButton = document.getElementById('saveButton');
 let isDrawing = false;
+let eraseMode = false;
 
 function createTable(rows, columns) {
     for (let i = 0; i < rows; i++) {
@@ -8,6 +11,7 @@ function createTable(rows, columns) {
         for (let j = 0; j < columns; j++) {
             const cell = document.createElement('td');
             cell.addEventListener('mousemove', handleCellHover);
+            cell.addEventListener('click', handleCellClick);
             row.appendChild(cell);
         }
         table.appendChild(row);
@@ -16,13 +20,61 @@ function createTable(rows, columns) {
 
 function handleCellHover(e) {
     if (isDrawing) {
-        e.target.style.backgroundColor = 'black';
+        if (eraseMode) {
+            e.target.style.backgroundColor = 'white';
+        } else {
+            e.target.style.backgroundColor = 'black';
+        }
+    }
+}
+
+function handleCellClick(e) {
+    if (eraseMode) {
+        e.target.style.backgroundColor = 'white';
     }
 }
 
 resetButton.addEventListener('click', function () {
     const cells = document.querySelectorAll('td');
     cells.forEach(cell => cell.style.backgroundColor = 'white');
+    eraseMode = false;
+    eraseButton.innerText = 'Prendre la gomme';
+});
+
+eraseButton.addEventListener('click', () => {
+    eraseMode = !eraseMode;
+    if (eraseMode) {
+        eraseButton.innerText = 'Poser la gomme';
+    } else {
+        eraseButton.innerText = 'Prendre la gomme';
+    }
+});
+
+saveButton.addEventListener('click', function () {
+    const blackCells = document.querySelectorAll('td[style*="background-color: black"]');
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const svgNS = svg.namespaceURI;
+
+    svg.setAttribute('width', '800'); 
+    svg.setAttribute('height', '800');
+
+    blackCells.forEach(cell => {
+        const rect = document.createElementNS(svgNS, 'rect');
+        rect.setAttribute('x', cell.cellIndex * 40);
+        rect.setAttribute('y', cell.parentElement.rowIndex * 40);
+        rect.setAttribute('width', '40');
+        rect.setAttribute('height', '40');
+        rect.setAttribute('fill', 'black');
+        svg.appendChild(rect);
+    });
+
+    const svgBlob = new Blob([svg.outerHTML], {type: 'image/svg+xml'});
+    const url = URL.createObjectURL(svgBlob);
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'my_drawing.svg';
+    link.click();
 });
 
 table.addEventListener('mousedown', () => {
@@ -33,4 +85,4 @@ table.addEventListener('mouseup', () => {
     isDrawing = false;
 });
 
-createTable(50, 50); // cases du tableau
+createTable(20, 20);
